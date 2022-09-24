97a6394a6b4f0d06201e7a72aa2179366125a707
Moamin <moamin.salamah@gmail.com>
2022-09-24 09:57:48 +0200

    deplying

---
+++ js/about.92b6c911.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkagenda_calender_vue"]=self["webpackChunkagenda_calender_vue"]||[]).push([[443],{1272:function(e,n,u){u.r(n),u.d(n,{default:function(){return c}});var a=function(){var e=this;e._self._c;return e._m(0)},t=[function(){var e=this,n=e._self._c;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],s=u(1001),l={},r=(0,s.Z)(l,a,t,!1,null,null,null),c=r.exports}}]);
+//# sourceMappingURL=about.92b6c911.js.map
